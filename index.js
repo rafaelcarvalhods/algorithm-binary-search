c3689fa02970df160f5eb73a05c32bd745db8290
Rafael Carvalho <rafaelcarvalho.rl@gmail.com>
2023-02-19 12:48:55 -0400

    Second try - Successful

--- index.js
+++ index.js
@@ -6,27 +6,32 @@
 // 6. If the target element is greater than the element at the middle index, set the low index to the middle index + 1.
 // 7. Repeat steps 3-6 until the element is found or it is clear that the element is not present in the array.
 
-let list = []
+
 function binarySearch(list, target, low, high) {
     low = 0;
-    high = list.length - 1
+    high = list.length
+    console.log(list.length)
 
     while (low <= high) {
-        middle = (low + high) /2
+
+        middle = low + Math.floor((high - low) /2)
+        console.log(middle)
+
         if(target == list[middle]){
             return list[middle]
         } else if(target < list[middle]){
-            high = list[middle] - 1 
+            high = middle - 1 
         } else {
-            low = list[middle] + 1
+            low = middle + 1
         }
     }
 
     return -1
 }
 
-myList = [1, 2, 3, 9, 10, 11, 17, 20]
+myList = [1, 2, 3, 9, 10, 11, 17, 20, 21]
 result = binarySearch(myList, 20)
+console.log(result)
 if(result == -1){
     console.log("The item is not on the list")
 } else {
